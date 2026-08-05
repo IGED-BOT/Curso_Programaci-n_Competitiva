e00df0de3309d5c3732510e5eb9d6baa99bc2f0d
Edy Ignacio <ignaciofed58@gmail.com>
2026-08-05 08:58:32 -0500

    Add backtracking solution for ETF portfolio problem
    Implement backtracking algorithm to count ETF combinations.

--- IEEE-CS-ProblemSet-2026/E_ETF_Portfolio/E.cpp
+++ IEEE-CS-ProblemSet-2026/E_ETF_Portfolio/E.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+int C, N;
+int precio[25];
+int respuesta = 0;
+
+void backtracking(int pos, int suma) {
+
+    if (suma > C)
+        return;
+
+    if (pos == N) {
+        if (suma == C)
+            respuesta++;
+        return;
+    }
+
+    // No comprar este ETF
+    backtracking(pos + 1, suma);
+
+    // Comprar este ETF
+    backtracking(pos + 1, suma + precio[pos]);
+}
+
+int main() {
+
+    cin >> C >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> precio[i];
+
+    backtracking(0, 0);
+
+    cout << respuesta << endl;
+
+    return 0;
+}

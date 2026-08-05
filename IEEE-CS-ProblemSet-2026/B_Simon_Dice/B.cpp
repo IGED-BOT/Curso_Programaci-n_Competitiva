9d4db484540663b81182d1c827801dc43250b236
Edy Ignacio <ignaciofed58@gmail.com>
2026-08-05 08:55:40 -0500

    Update B.cpp

--- IEEE-CS-ProblemSet-2026/B_Simon_Dice/B.cpp
+++ IEEE-CS-ProblemSet-2026/B_Simon_Dice/B.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    string s;
+    string inicio = "Simon says ";
+
+    for (int i = 0; i < n; i++) {
+        getline(cin, s);
+
+        if (s.substr(0, inicio.size()) == inicio) {
+            cout << s.substr(inicio.size()) << endl;
+        }
+    }
+
+    return 0;
+}

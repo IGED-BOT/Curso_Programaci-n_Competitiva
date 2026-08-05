9f55ca83c7e90c155d2dd1977dd23135a229c898
Edy Ignacio <ignaciofed58@gmail.com>
2026-08-05 08:55:19 -0500

    Implement sorting and character mapping in A.cpp

--- IEEE-CS-ProblemSet-2026/A_ABC/A.cpp
+++ IEEE-CS-ProblemSet-2026/A_ABC/A.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main() {
+    int v[3];
+    cin >> v[0] >> v[1] >> v[2];
+
+    sort(v, v + 3);
+
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < 3; i++) {
+        if (s[i] == 'A')
+            cout << v[0];
+        else if (s[i] == 'B')
+            cout << v[1];
+        else
+            cout << v[2];
+
+        if (i < 2)
+            cout << " ";
+    }
+
+    return 0;
+}

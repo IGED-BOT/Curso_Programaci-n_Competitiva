95c805f467b381c6ab557551cae04d5e2d1e6083
Edy Ignacio <ignaciofed58@gmail.com>
2026-08-05 08:57:36 -0500

    Update D.cpp

--- IEEE-CS-ProblemSet-2026/D_One_on_One_Basketball/D.cpp
+++ IEEE-CS-ProblemSet-2026/D_One_on_One_Basketball/D.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string s;
+    cin >> s;
+
+    int A = 0, B = 0;
+
+    for (int i = 0; i < s.size(); i += 2) {
+
+        if (s[i] == 'A')
+            A += s[i + 1] - '0';
+        else
+            B += s[i + 1] - '0';
+
+        if (A >= 10 && B >= 10) {
+            if (A - B >= 2) {
+                cout << "A";
+                return 0;
+            }
+            if (B - A >= 2) {
+                cout << "B";
+                return 0;
+            }
+        } else {
+            if (A >= 11) {
+                cout << "A";
+                return 0;
+            }
+            if (B >= 11) {
+                cout << "B";
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}

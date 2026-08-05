90fe1e2b86c1e38efcf6ece8df0cf74db284cb04
Edy Ignacio <ignaciofed58@gmail.com>
2026-08-05 08:57:07 -0500

    Update C.cpp

--- IEEE-CS-ProblemSet-2026/C_ACM_Scoring/C.cpp
+++ IEEE-CS-ProblemSet-2026/C_ACM_Scoring/C.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
 
+int main() {
+    map<char, int> wrong;
+    map<char, bool> solved;
+
+    int resueltos = 0;
+    int tiempo = 0;
+
+    while (true) {
+        int minuto;
+        cin >> minuto;
+
+        if (minuto == -1)
+            break;
+
+        char problema;
+        string resultado;
+
+        cin >> problema >> resultado;
+
+        if (solved[problema])
+            continue;
+
+        if (resultado == "wrong") {
+            wrong[problema]++;
+        } else {
+            solved[problema] = true;
+            resueltos++;
+            tiempo += minuto + wrong[problema] * 20;
+        }
+    }
+
+    cout << resueltos << " " << tiempo << endl;
+
+    return 0;
+}

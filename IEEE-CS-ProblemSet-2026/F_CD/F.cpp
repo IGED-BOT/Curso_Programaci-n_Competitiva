ffe60b7882e61e7bd63e8bad0b3ed4363bef5e94
Edy Ignacio <ignaciofed58@gmail.com>
2026-08-05 08:58:58 -0500

    Implement common elements counting in arrays

--- IEEE-CS-ProblemSet-2026/F_CD/F.cpp
+++ IEEE-CS-ProblemSet-2026/F_CD/F.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int N, M;
+
+    while (cin >> N >> M) {
+
+        if (N == 0 && M == 0)
+            break;
+
+        int *jack = new int[N];
+        int *jill = new int[M];
+
+        for (int i = 0; i < N; i++)
+            cin >> jack[i];
+
+        for (int i = 0; i < M; i++)
+            cin >> jill[i];
+
+        int i = 0, j = 0;
+        int comunes = 0;
+
+        while (i < N && j < M) {
+
+            if (jack[i] == jill[j]) {
+                comunes++;
+                i++;
+                j++;
+            }
+            else if (jack[i] < jill[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        cout << comunes << endl;
+
+        delete[] jack;
+        delete[] jill;
+    }
+
+    return 0;
+}
